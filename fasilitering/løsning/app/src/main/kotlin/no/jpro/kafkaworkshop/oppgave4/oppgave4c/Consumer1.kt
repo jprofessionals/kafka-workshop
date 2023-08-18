399f34ec5899f42cb480442049f4cf757eaae4c3
joar aurdal <joaraurdal@joars-MacBook-Pro.local>
2023-08-18 16:49:00 +0200

    Legg til filter på consumer, slik at den bare prosesseserer meldinger som den er ment å prosessere

--- fasilitering/løsning/app/src/main/kotlin/no/jpro/kafkaworkshop/oppgave4/oppgave4c/Consumer1.kt
+++ fasilitering/løsning/app/src/main/kotlin/no/jpro/kafkaworkshop/oppgave4/oppgave4c/Consumer1.kt
@@ -1,4 +1,5 @@
 import com.fasterxml.jackson.databind.node.JsonNodeFactory
+import no.jpro.kafkaworkshop.oppgave4.oppgave4a.MessageData
 import no.jpro.kafkaworkshop.oppgave4.oppgave4a.Rapid
 import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.clients.consumer.KafkaConsumer
@@ -19,7 +20,8 @@ fun main() {
         ConsumerConfig.MAX_POLL_INTERVAL_MS_CONFIG to "6000",
         ConsumerConfig.HEARTBEAT_INTERVAL_MS_CONFIG to "1000",
 
-    )
+        )
+
 
     val consumerId1Mapping = mapOf("10" to "A14", "11" to "B55", "12" to "H2", "13" to "X91", "14" to "V20")
 
@@ -32,27 +34,49 @@ fun main() {
                 logger.info("Consumed record with key ${record.key()} and value ${record.value()}")
                 val message: Rapid.RapidMessage? = Rapid.RapidMessage.MessageConverter().convertFromJson(record.value())
                 if (message != null) {
-                    logger.info("Deserialized Message: $message")
-                    val producer1Message = message.messageData
-                    val id = producer1Message["key1"]
-                    val consumerId1Key = consumerId1Mapping.get(id?.get("producer1ItemId")?.asText())
-                    val value = producer1Message["key2"]
+                    logger.info("Consumer 1 Deserialized Message: $message")
+                    val incomingMessage = message.messageData
 
-                    logger.info("Producer 1 message received: $id: id value: $value Mapping to Consumer1Id: $consumerId1Key")
+                    if (shouldProcessMessage(incomingMessage)) {
+                        val id = incomingMessage["key1"]
 
-                    val messageNodeFactory = JsonNodeFactory.instance
+                        logger.info("Consumer 1 Processing message: $message")
 
-                    val newMessage = message.copy2(
-                        Rapid.RapidMessage.ParticipatingSystem("consumer1"), mapOf(
-                            "consumer1Id" to messageNodeFactory.objectNode().put("id", consumerId1Key)
+                        val consumerId1Key = consumerId1Mapping.get(id?.get("producer1ItemId")?.asText())
+                        val value = incomingMessage["key2"]
+
+                        logger.info("Producer 1 message received: $id: id value: $value Mapping to Consumer1Id: $consumerId1Key")
+
+                        val messageNodeFactory = JsonNodeFactory.instance
+
+                        val newMessage = message.copy2(
+                            Rapid.RapidMessage.ParticipatingSystem("consumer1"), mapOf(
+                                "consumer1Id" to messageNodeFactory.objectNode().put("id", consumerId1Key)
+                            )
                         )
-                    )
 
-                    logger.info("newMessage: ${newMessage.toJsonText()}")
+                        logger.info("newMessage: ${newMessage.toJsonText()}")
+
+                        val willNextMessageBeConsumedAgain = shouldProcessMessage(newMessage.messageData)
+                        if(willNextMessageBeConsumedAgain) {
+                            logger.error("Can not create new message, it will be consumed again and create a loop")
+                        }
+                    }
                 } else {
                     logger.error("Error deserializing record value: ${record.value()}")
                 }
             }
         }
     }
+
+
+}
+
+fun shouldProcessMessage(incomingMessage: MessageData): Boolean {
+    val producer1Id = incomingMessage["key1"]
+    val harProducer1Id = producer1Id != null && !producer1Id.isNull
+    val consumer1Id = incomingMessage["consumer1Id"]
+    val manglerConsumer1Id = consumer1Id != null && !consumer1Id.isNull
+
+    return harProducer1Id && manglerConsumer1Id
 }
